8fc9dad5360ec28d2b1bed8b2efb6dd527781df2
tonygeorose <86162127+tonygeorose@users.noreply.github.com>
2021-06-20 13:14:29 +0530

    Update mttUserAuthServices.js

--- mttUserAuthServices.js
+++ mttUserAuthServices.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 // New app using express module
 const app = express();
